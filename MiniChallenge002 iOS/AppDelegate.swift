8e7beae5f9fd61da65f2dacb9e442bea58df1291
oManhattan <matheusbatuque@gmail.com>
2022-12-06 21:21:24 -0300

    Música adicionada

--- MiniChallenge002 iOS/AppDelegate.swift
+++ MiniChallenge002 iOS/AppDelegate.swift
@@ -11,8 +11,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var applicationWillEnterForegroundAction: (() -> Void)?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
